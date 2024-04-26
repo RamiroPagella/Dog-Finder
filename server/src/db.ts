d886257870449b022b16381cd8d3d9a9a60e6fa6
RamiroPagella <ramiro.s.pagella@gmail.com>
2024-04-26 14:54:46 -0300

    conexion a base de datos en render

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,18 +1,30 @@
-import { Sequelize } from 'sequelize-typescript';
-import DogModel from './models/Dog.model';
-import UserModel from './models/User.model';
-import LikesModel from './models/Likes.model';
-import DogPendingModel from './models/DogPending.model';
+import { Sequelize } from "sequelize-typescript";
+import { config } from "dotenv";
+import DogModel from "./models/Dog.model";
+import UserModel from "./models/User.model";
+import LikesModel from "./models/Likes.model";
+import DogPendingModel from "./models/DogPending.model";
 
+config();
+
+// const conn = new Sequelize({
+//   host: "localhost",
+//   dialect: "postgres",
+//   username: "postgres",
+//   password: 'r61889079',
+//   database: "dogs",
+//   logging: false,
+//   models: [DogModel, UserModel, LikesModel, DogPendingModel]
+// })
 
 const conn = new Sequelize({
-  host: "localhost",
+  host: process.env.DB_HOST,
   dialect: "postgres",
-  username: "postgres",
-  password: 'r61889079',
-  database: "dogs",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "dog-finder-database",
   logging: false,
-  models: [DogModel, UserModel, LikesModel, DogPendingModel]
-})
+  models: [DogModel, UserModel, LikesModel, DogPendingModel],
+});
 
 export default conn;
